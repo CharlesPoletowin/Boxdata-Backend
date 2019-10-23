0bb673a6e5ea00a9bec875e869a786a684dfa655
CharlesPoletoWin <1409985968@qq.com>
2019-10-23 16:44:33 +0800

    adjust the data to frontend especially trail echarts

--- demo/localDB.py
+++ demo/localDB.py
@@ -3,8 +3,10 @@
 # file   ：localDB.PY
 # PRODUCT_NAME  ：PyCharm
 
+import numpy as np
 import pymysql
-config={'host':'localhost','user':'root','password':'111111','database':'monitor','charset':'utf8','port':3306}
+import datetime
+config={'host':'10.200.43.40','user':'root','password':'1','database':'monitor','charset':'utf8','port':3306}
 
 
 
@@ -21,7 +23,8 @@ def select_new_data():
         IsNormal = []
         for row in data:
             value.append(float(row[0]))
-            time.append(row[1])
+            time.append((datetime.datetime(2019,10,23,10,29,5)-row[1]).seconds+1)
+            # time.append((datetime.datetime.now()-row[1]).seconds+1)
             IsNormal.append(row[2])
         value.reverse()
         time.reverse()
@@ -30,9 +33,16 @@ def select_new_data():
         result['time'] = time
         result['IsNormal'] = IsNormal
         # print(result)
+        if str2 == "locationx":
+            locationx.append(value)
+            locationx.append(time)
+        elif str2 == "locationy":
+            locationy.append(value)
         data_result_list.append(result)
 
     data_result_list = []
+    locationx = []
+    locationy = []
     connection = pymysql.connect(**config)
     cur = connection.cursor()
     get_new("pressure")
@@ -44,7 +54,10 @@ def select_new_data():
     # print(data_result_list[2]['time'])
     cur.close()
     connection.close()
-    # print(data_result_list)
+    trail = np.array([locationx[0],locationy[0],locationx[1]]).T.tolist()
+    traildict = {'monitor':"trail",'value':trail}
+    # print(traildict)
+    data_result_list.append(traildict)
     return data_result_list
 
 
--- init.sql
+++ init.sql
@@ -1,23 +1,39 @@
 create database monitor;
 use monitor;
 create table pressure (v decimal(7,3), ts datetime primary key, IsNormal boolean,IsChecked boolean);
-create table temperature1(v decimal(5,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
-create table humidity(v decimal(5,3),ts datetime primary key,IsNormal boolean,IsChecked boolean);
-create table locationx(v decimal(5,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
-create table locationy(v decimal(5,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
-create table vibration(v decimal(5,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
-create table i(v decimal(5,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
-create table u(v decimal(7,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
-create table voice(v decimal(5,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
+create table temperature1(v decimal(10,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
+create table humidity(v decimal(10,3),ts datetime primary key,IsNormal boolean,IsChecked boolean);
+create table locationx(v decimal(10,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
+create table locationy(v decimal(10,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
+create table vibration(v decimal(10,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
+create table i(v decimal(10,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
+create table u(v decimal(10,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
+create table voice(v decimal(10,3),ts datetime primary key, IsNormal boolean,IsChecked boolean);
 
 INSERT INTO `monitor`.`i` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('0.1', '2019:10:15 13:00', '1', '0');
 INSERT INTO `monitor`.`i` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('0.1', '2019-10-15 13:01', '1', '0');
 
 INSERT INTO `monitor`.`locationx` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('2', '2019-10-15 13:00:00', '1', '0');
 INSERT INTO `monitor`.`locationx` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('3', '2019-10-15 13:01:00', '1', '0');
+UPDATE `monitor`.`locationx` SET `ts` = '2019-10-23 10:29:00' WHERE (`ts` = '2019-10-15 13:00:00');
+UPDATE `monitor`.`locationx` SET `ts` = '2019-10-23 10:29:01' WHERE (`ts` = '2019-10-15 13:01:00');
+INSERT INTO `monitor`.`locationx` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('4', '2019-10-23 10:29:02', '1', '0');
+INSERT INTO `monitor`.`locationx` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('5', '2019-10-23 10:29:03', '1', '0');
+INSERT INTO `monitor`.`locationx` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('4', '2019-10-23 10:29:04', '1', '0');
+INSERT INTO `monitor`.`locationx` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('3', '2019-10-23 10:29:05', '1', '0');
+
+
 
 INSERT INTO `monitor`.`locationy` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('5', '2019-10-15 13:00:00', '1', '0');
 INSERT INTO `monitor`.`locationy` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('4', '2019-10-15 13:00:10', '1', '0');
+UPDATE `monitor`.`locationy` SET `ts` = '2019-10-23 10:29:00' WHERE (`ts` = '2019-10-15 13:00:00');
+UPDATE `monitor`.`locationy` SET `ts` = '2019-10-23 10:29:01' WHERE (`ts` = '2019-10-15 13:01:00');
+INSERT INTO `monitor`.`locationy` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('4', '2019-10-23 10:29:02', '1', '0');
+INSERT INTO `monitor`.`locationy` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('5', '2019-10-23 10:29:03', '1', '0');
+INSERT INTO `monitor`.`locationy` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('4', '2019-10-23 10:29:04', '1', '0');
+INSERT INTO `monitor`.`locationy` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('3', '2019-10-23 10:29:05', '1', '0');
+
+
 
 INSERT INTO `monitor`.`pressure` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('200', '2019-10-15 13:00:10', '1', '0');
 INSERT INTO `monitor`.`pressure` (`v`, `ts`, `IsNormal`, `IsChecked`) VALUES ('200', '2019-10-15 13:01:10', '1', '0');
