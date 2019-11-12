80fcd5a8af2af96a796c22d6f43e5aa484e27234
CharlesPoletoWin <1409985968@qq.com>
2019-11-12 15:23:02 +0800

    update address and adjust other former of the files

--- flask_socket/localDB.py
+++ flask_socket/localDB.py
@@ -6,7 +6,7 @@
 import numpy as np
 import pymysql
 
-config = {'host': '210.14.69.108', 'user': 'root', 'password': '1', 'database': 'iot', 'charset': 'utf8', 'port': 3306}
+config = {'host': '10.200.43.149', 'user': 'root', 'password': '1', 'database': 'iot', 'charset': 'utf8', 'port': 3306}
 
 
 temp = ""
@@ -15,7 +15,7 @@ temp = ""
 # get new data in order to send to frontend
 def select_new_data():
     def demo():
-        sql22 = "select x, y, time, isNormal from location where x is not null and y is not null order by time desc limit 10"
+        sql22 = "select x, y, time, isNormal from location where x is not null and y is not null and x >= 0 and x < 310 and y >= 0 and y < 310 order by time desc limit 100"
         cur.execute(sql22)
         data = cur.fetchall()
         normal = []
@@ -29,15 +29,14 @@ def select_new_data():
                 normal.append([row[0], row[1], ((temp - row[2] ).seconds + 1)])
             else:
                 abnormal.append([row[0], row[1], ((temp - row[2]).seconds + 1)])
-
-            if len(normal) >= 1:
-                t1 = np.array(normal)
-                t1[:, 2] = 12 - t1[:, 2]
-                normal = t1.tolist()
-            if len(abnormal) >= 1:
-                t2 = np.array(abnormal)
-                t2[:, 2] = 12 - t2[:, 2]
-                abnormal = t2.tolist()
+        if len(normal) >= 1:
+            t1 = np.array(normal)
+            t1[:, 2] = 101 - t1[:, 2]
+            normal = t1.tolist()
+        if len(abnormal) >= 1:
+            t2 = np.array(abnormal)
+            t2[:, 2] = 101 - t2[:, 2]
+            abnormal = t2.tolist()
         data1 = [normal, abnormal]
         data_result_list["location"] = data1
 
@@ -65,11 +64,44 @@ def select_new_data():
         if num > 1:
             if len(normal) >= 1:
                 t1 = np.array(normal)
-                t1[:, 0] = 12-t1[:, 0]
+                t1[:, 0] = 1+num-t1[:, 0]
                 normal = t1.tolist()
             if len(abnormal) >= 1:
                 t2 = np.array(abnormal)
-                t2[:, 0] = 12-t2[:, 0]
+                t2[:, 0] = 1+num-t2[:, 0]
+                abnormal = t2.tolist()
+        data1 = [normal, abnormal]
+        data_result_list[str2] = data1
+
+    def get_new2(str2, num=1):
+        sql = "select value, time, isNormal from "+str(str2)+" where value >= 0 and value <= 310 order by time desc limit " + str(num)
+        cur.execute(sql)
+        data = cur.fetchall()
+        normal = []
+        abnormal = []
+        global temp
+        temp = ""
+        for row in data:
+            if temp == "":
+                temp = row[1]
+            if num == 1:
+                if row[2] == 0:
+                    normal.append([float(row[0]), ((temp-row[1]).seconds+1)])
+                else:
+                    abnormal.append([float(row[0]), ((temp-row[1]).seconds+1)])
+            else:
+                if row[2] == 0:
+                    normal.append([((temp-row[1]).seconds+1), float(row[0])])
+                else:
+                    abnormal.append([((temp-row[1]).seconds+1), float(row[0])])
+        if num > 1:
+            if len(normal) >= 1:
+                t1 = np.array(normal)
+                t1[:, 0] = 1+num-t1[:, 0]
+                normal = t1.tolist()
+            if len(abnormal) >= 1:
+                t2 = np.array(abnormal)
+                t2[:, 0] = 1+num-t2[:, 0]
                 abnormal = t2.tolist()
         data1 = [normal, abnormal]
         data_result_list[str2] = data1
@@ -77,17 +109,18 @@ def select_new_data():
     data_result_list = {}
     connection = pymysql.connect(**config)
     cur = connection.cursor()
-    get_new("temperature",1)
-    get_new("humidity")
-    get_new("speedx",5)
-    get_new("speedy",5)
-    get_new("locationx",10)
-    get_new("locationy",10)
+    get_new("temperature")
+    get_new("noise")
+    get_new("speedx",20)
+    get_new("speedy",20)
+    get_new2("locationx",40)
+    get_new2("locationy",40)
     demo()
-    get_new("vibration",10)
+    get_new("vibration",20)
 
     cur.close()
     connection.close()
+    print(data_result_list)
     return data_result_list
 
 
